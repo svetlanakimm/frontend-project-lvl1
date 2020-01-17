0c767e37d8559eefbaa828d37df5f317f879eb7b
Svetlana Kim <s.kim@sport-express.ru>
2020-01-17 15:29:16 +0300

    Even game implemented, asciinema video added to README.md

--- README.md
+++ README.md
@@ -6,25 +6,6 @@
 
 В рамках проекта необходимо реализовать набор мини-игр, запускаемых из консоли.
 
-Пример игры:
+Установка пакета и запуск игры:
 
-```
-$ brain-progression
-
-Welcome to the Brain Game!
-What number is missing in this progression?
-
-May I have your name? Roman
-Hello, Roman!
-
-Question: 14 .. 18 20 22 24 26 28
-Your answer: 16
-Correct!
-Question: 5 6 7 8 9 .. 11 12
-Your answer: 10
-Correct!
-Question: 12 15 18 21 .. 27 30 33
-Your answer: 24
-Correct!
-Congratulations, Roman!
-```
+[![asciicast](https://asciinema.org/a/ToqttAUJotYYdiNJn3KzYoMFX.svg)](https://asciinema.org/a/ToqttAUJotYYdiNJn3KzYoMFX)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 #!/usr/bin/env node
 
 const readlineSync = require('readline-sync');
+const MINIMUM = 1;
+const MAXIMUM = 100;
+const ROUNDS = 3;
 
 const Init = () => {
-  const userName = readlineSync.question('May I have your name? ');
-
   console.log('Welcome to the Brain Games!');
-  // Wait for user's response.
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hi, ${userName}!`);
+  const randomInt = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1);
+    return Math.floor(rand);
+  };
+  for (let i = 0; i < ROUNDS; i += 1) {
+    const randomNum = randomInt(MINIMUM, MAXIMUM);
+    console.log(`Question: ${randomNum}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const isEven = (num) => {
+      if ((num % 2) === 0) {
+        return 'yes';
+      }
+      return 'no';
+    };
+    const rightAnswer = isEven(randomNum);
+    if (rightAnswer === userAnswer) {
+      if (i === 2) {
+        console.log(`Congratulations, ${userName}!`);
+      } else {
+        console.log('Correct!');
+      }
+    } else {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
 };
 export default Init;
